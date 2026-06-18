46ad063a592aa28b458959c49868d7808da4d199
AlexNovitchkovBurbank <ryzen71511@outlook.com>
2026-06-17 22:50:18 -0700

    fixed

--- src/s3Interfaces/getJson.ts
+++ src/s3Interfaces/getJson.ts
@@ -5,7 +5,20 @@ const cloudfrontDistroUri = import.meta.env.VITE_BASE_URL;
 export const getJson = async (folder: string, pageName: string) => {
   const projectDataUrl = `https://${cloudfrontDistroUri}/${folder}/${pageName}.json`;
 
-  return axios.get(projectDataUrl);
+  try {
+    return await axios.get(projectDataUrl);
+  } catch (err: any) {
+    console.error("getJson fetch failed", {
+      projectDataUrl,
+      folder,
+      pageName,
+      status: err?.response?.status,
+      headers: err?.response?.headers,
+      data: err?.response?.data,
+      message: err?.message,
+    });
+    throw err;
+  }
 };
 
 export default getJson;
